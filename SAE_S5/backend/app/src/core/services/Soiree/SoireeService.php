836ad3b193f04f35f9910151556ecfc67321cf40
Gaetan66 <127093056+Gaetan66@users.noreply.github.com>
2024-10-21 14:14:51 +0200

    ajout database

--- SAE_S5/backend/app/src/core/services/Soiree/SoireeService.php
+++ SAE_S5/backend/app/src/core/services/Soiree/SoireeService.php
@@ -1,9 +1,16 @@
 <?php
 
+use nrv\core\dto\SoireeDTO;
+use repositoryInterfaces\SoireeRepositoryInterface;
+
 class SoireeService implements SoireeServiceInterface
 {
 
+    private SoireeRepositoryInterface $soireeRepository;
 
+    public function __construct(SoireeRepositoryInterface $s){
+        $this->soireeRepository = $s;
+    }
 
     public function afficherSoiree(string $ID): SoireeDTO
     {
--- SAE_S5/backend/app/src/infrastrucure/repositories/SoireeRepository.php
+++ SAE_S5/backend/app/src/infrastrucure/repositories/SoireeRepository.php
@@ -2,5 +2,23 @@
 
 class SoireeRepository implements \repositoryInterfaces\SoireeRepositoryInterface
 {
+    private PDO $pdo;
+
+
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function getSoireeByID(int $id){
+        $stmt = $this->pdo->prepare("SELECT * FROM soirees WHERE id = :id");
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+
+
 
 }
