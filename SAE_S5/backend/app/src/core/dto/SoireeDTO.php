4c1dc6a53da1a9674ba6ed47e692deca38f993b9
jeandidi3u <jeandidier.clement@gmail.com>
2024-10-21 11:18:06 +0200

    ajout DTO

---
+++ SAE_S5/backend/app/src/core/dto/SoireeDTO.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace nrv\core\dto;
+
+use Faker\Core\DateTime;
+use nrv\core\domain\entities\Soiree\Soiree;
+use nrv\core\dto\DTO;
+
+class SoireeDTO extends DTO{
+    protected string $ID;
+    protected string $nom;
+    protected string $thematique;
+
+    protected DateTime $dateSoiree;
+    protected string $lieuSoiree;
+    protected int $nbPlaces;
+    protected float $tarif;
+    protected float $tarifReduit;
+
+    public function __construct(Soiree $s){
+        $this->ID = $s->getID();
+        $this->nom = $s->nom;
+        $this->thematique = $s->thematique;
+        $this->dateSoiree = $s->dateSoiree;
+        $this->lieuSoiree = $s->lieuSoiree;
+        $this->nbPlaces = $s->nbPlaces;
+        $this->tarif = $s->tarif;
+        $this->tarifReduit = $s->tarifReduit;
+    }
+
+
+
+
+}
