f544da45de0ddf3a6984b5b2060a08b40e826192
Gaetan66 <127093056+Gaetan66@users.noreply.github.com>
2024-10-22 15:14:38 +0200

    CORS

--- SAE_S5/backend/app/config/routes.php
+++ SAE_S5/backend/app/config/routes.php
@@ -6,5 +6,12 @@ use Psr\Http\Message\ResponseInterface as Response;
 
 return function( \Slim\App $app):\Slim\App {
     $app->get('/soiree/{ID-SOIREE}', nrv\application\action\GetSoireeByIDAction::class);
+
+    $app->options('/{routes:.+}',
+        function( Request $rq,
+                  Response $rs, array $args) : Response {
+            return $rs;
+        })->add(new \nrv\application\middleware\CorsMiddleware());
+
     return $app;
 };
