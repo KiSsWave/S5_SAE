a79c36bbd37792dd15ec00d0fd2941b4e38d634c
jeandidi3u <jeandidier.clement@gmail.com>
2024-10-23 15:14:51 +0200

    evolv

---
+++ SAE_S5/backend/app/src/core/dto/BilletDTO.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace nrv\core\dto;
+
+use nrv\core\domain\entities\Soiree\Billet;
+use nrv\core\dto\DTO;
+
+class BilletDTO extends DTO
+{
+    protected string $ID;
+
+    protected string $ID_acheteur;
+    protected string $nomAcheteur;
+    protected string $reference;
+
+    protected string $dateHoraireSoiree;
+    protected string $typeTarif;
+
+    protected int $prix;
+
+    public function __construct(Billet $billet, string $ID_acheteur)
+    {
+        $this->ID = $billet->getID();
+        $this->ID_acheteur = $ID_acheteur;
+        $this->nomAcheteur = $billet->nomAcheteur;
+        $this->reference = $billet->reference;
+        $this->dateHoraireSoiree = $billet->dateHoraireSoiree;
+        $this->typeTarif = $billet->typeTarif;
+        $this->prix = $billet->prix;
+    }
+}
--- SAE_S5/backend/app/src/infrastructure/repositories/SpectacleRepository.php
+++ SAE_S5/backend/app/src/infrastructure/repositories/SpectacleRepository.php
@@ -3,8 +3,10 @@
 namespace nrv\infrastructure\repositories;
 
 use DateTime;
+use nrv\core\domain\entities\Soiree\Billet;
 use nrv\core\domain\entities\Spectacle\Spectacle;
 use nrv\core\domain\entities\Spectacle\SpectacleSoiree;
+use nrv\core\dto\BilletDTO;
 use nrv\infrastructure\DatabaseConnection;
 use nrv\core\repositoryInterfaces\SpectacleRepositoryInterface;
 use nrv\core\domain\entities\Spectacle\SpectacleArtiste;
@@ -16,21 +18,26 @@ class SpectacleRepository implements SpectacleRepositoryInterface
 
     private array $spectacles = [];
 
-
     public function __construct()
     {
         $this->pdo = DatabaseConnection::getPDO('nrv');
         $spectacles = $this->pdo->query("SELECT * FROM spectacles")->fetchAll(PDO::FETCH_ASSOC);
         foreach ($spectacles as $spectacle) {
             $horaire = new DateTime($spectacle['horaire']);
-            $this->spectacles[$spectacle['id']] = new Spectacle($spectacle['titre'], $spectacle['description'],$spectacle['images'], $spectacle['urlvideo'], $spectacle['style'], $horaire );
+            $this->spectacles[$spectacle['id']] = new Spectacle(
+                $spectacle['titre'],
+                $spectacle['description'],
+                $spectacle['images'],
+                $spectacle['urlvideo'],
+                $spectacle['style'],
+                $horaire
+            );
             $this->spectacles[$spectacle['id']]->setID($spectacle['id']);
         }
-
     }
 
-
-    public function getSpectacleByID(int $id){
+    public function getSpectacleByID(int $id)
+    {
         $stmt = $this->pdo->prepare("SELECT * FROM spectacles WHERE id = :id");
         $stmt->bindValue(':id', $id);
         $stmt->execute();
@@ -43,7 +50,8 @@ class SpectacleRepository implements SpectacleRepositoryInterface
         return $this->spectacles;
     }
 
-    public function SoireBySpectaclesID(string $id){
+    public function SoireBySpectaclesID(string $id)
+    {
         $stmt = $this->pdo->prepare("SELECT * FROM SPECTACLESOIREE WHERE id_spectacle = :id_spectacle");
         $stmt->bindValue(':id_spectacle', $id);
         $stmt->execute();
@@ -56,12 +64,10 @@ class SpectacleRepository implements SpectacleRepositoryInterface
         }
 
         return $soiree;
-
     }
 
     public function getSpectaclesFiltres(?DateTime $date = null, ?string $style = null, ?string $lieu = null): array
     {
-
         $query = "SELECT s.*, so.lieusoiree FROM spectacles s
               JOIN SPECTACLESOIREE ss ON s.id = ss.id_spectacle
               JOIN soirees so ON ss.id_soiree = so.id
@@ -94,7 +100,7 @@ class SpectacleRepository implements SpectacleRepositoryInterface
         $filteredSpectacles = [];
 
         foreach ($spectacles as $spectacle) {
-            if (isset($spectacle['id'])) { 
+            if (isset($spectacle['id'])) {
                 $horaire = new DateTime($spectacle['horaire']);
                 $filteredSpectacles[] = new Spectacle(
                     $spectacle['titre'],
@@ -104,14 +110,15 @@ class SpectacleRepository implements SpectacleRepositoryInterface
                     $spectacle['style'],
                     $horaire
                 );
-                $filteredSpectacles[count($filteredSpectacles) - 1]->setID($spectacle['id']); // Ajoutez l'ID
+                $filteredSpectacles[count($filteredSpectacles) - 1]->setID($spectacle['id']);
             }
         }
 
         return $filteredSpectacles;
     }
 
-    public function getSoireeBySpectacleID(string $id){
+    public function getSoireeBySpectacleID(string $id)
+    {
         $stmt = $this->pdo->prepare("SELECT * FROM SPECTACLESOIREE WHERE id_spectacle = :id_spectacle");
         $stmt->bindValue(':id_spectacle', $id);
         $stmt->execute();
@@ -124,11 +131,10 @@ class SpectacleRepository implements SpectacleRepositoryInterface
         }
 
         return $soirees;
-
-
     }
 
-    public function getArtisteBySpectacleID(string $id){
+    public function getArtisteBySpectacleID(string $id)
+    {
         $stmt = $this->pdo->prepare("SELECT * FROM ARTISTESPECTACLE natural join ARTISTES WHERE id_spectacle = :id_spectacle");
         $stmt->bindValue(':id_spectacle', $id);
         $stmt->execute();
@@ -144,5 +150,25 @@ class SpectacleRepository implements SpectacleRepositoryInterface
     }
 
 
+    public function creerBillets(BilletDTO $billet): void
+    {
+        $sql = "
+            INSERT INTO billets (id, id_acheteur, nom_acheteur, reference, typetarif, prix, date_horaire_soiree)
+            VALUES (:id, :id_acheteur, :nom_acheteur, :reference, :typetarif, :prix, :date_horaire_soiree)
+        ";
 
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->bindValue(':id', $billet->getID());
+        $stmt->bindValue(':id_acheteur', $billet->ID_acheteur);
+        $stmt->bindValue(':nom_acheteur', $billet->nomAcheteur);
+        $stmt->bindValue(':reference', $billet->reference);
+        $stmt->bindValue(':typetarif', $billet->typeTarif);
+        $stmt->bindValue(':prix', $billet->prix);
+        $stmt->bindValue(':date_horaire_soiree', $billet->dateHoraireSoiree);
+
+        if (!$stmt->execute()) {
+            throw new \Exception('Erreur lors de la création du billet.');
+        }
+    }
 }
