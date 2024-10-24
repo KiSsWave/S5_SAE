daa0e43a3d094c2458755b4b0bb77f64076f0404
Okiles <colsonlenny5@gmail.com>
2024-10-24 14:51:53 +0200

    updating styles

--- SAE_S5/frontend/js/loader.js
+++ SAE_S5/frontend/js/loader.js
@@ -3,22 +3,13 @@ import conf from './config.js';
 const token = localStorage.getItem('token');
 
 async function loadSpectacle(url) {
-    return fetch(url, {
-        headers: {
-            'Authorization': 'Bearer ' + token
-        }
-    }).catch(error => {
+    return fetch(url).catch(error => {
         console.error('Erreur lors de la récupération du spectacle');
     });
 }
 
 async function loadSoiree(url) {
-    return fetch(url, {
-        method: 'GET',
-        headers: {
-            'Authorization': 'Bearer ' + token,
-        }
-    }).catch(error => {
+    return fetch(url).catch(error => {
         console.error('Erreur lors de la récupération de la soirée');
     });
 }
@@ -27,21 +18,13 @@ async function loadAllSpectacles(url, filter = '', value = '') {
     if (filter != 'none') {
         url += `?${filter}=${value}`;
     }
-    return fetch(url, {
-        headers: {
-            'Authorization': 'Bearer ' + token
-        }
-    }).catch(error => {
+    return fetch(url).catch(error => {
         console.error('Erreur lors de la récupération de la liste des spectacles');
     });
 }
 
 async function loadSpectacleBySoiree(url){
-    return fetch(url, {
-        headers: {
-            'Authorization': 'Bearer ' + token
-        }
-    }).catch(error=>{
+    return fetch(url).catch(error=>{
         console.error('Erreur de la récupération de la liste des spectacles')
     })
 }
