e9de1d86762619c71f7d3406997d765624adc2c0
Okiles <colsonlenny5@gmail.com>
2024-11-05 15:11:21 +0100

    Update GetCommandeAction.php

--- SAE_S5/backend/app/src/application/action/GetCommandeAction.php
+++ SAE_S5/backend/app/src/application/action/GetCommandeAction.php
@@ -31,9 +31,9 @@ class GetCommandeAction extends AbstractAction
             foreach ($commandes as $command) {
                 $tarif = 0;
                 if($command->typetarif == "tarif"){
-                    $tarif = $this->soireeService->afficherSoiree($command->ID)->tarif*$command->placesvendues;
+                    $tarif = $this->soireeService->afficherSoiree($command->idsoiree)->tarif*$command->placesvendues;
                 } else {
-                    $tarif = $this->soireeService->afficherSoiree($command->ID)->tarifReduit*$command->placesvendues;
+                    $tarif = $this->soireeService->afficherSoiree($command->idsoiree)->tarifReduit*$command->placesvendues;
                 }
 
                 $resultat['Commandes'][] = [
